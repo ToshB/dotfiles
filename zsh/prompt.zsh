a1a0c401f65108eaf1e92a4f7a503bb2d71fd5c9
Torstein Bjørnstad <tosh@tosh.no>
2012-09-30 16:09:03 +0200

    Removed todos from prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -42,44 +42,12 @@ need_push () {
   
 }
 
-rb_prompt(){
-  if $(which rbenv &> /dev/null)
-  then
-	  echo "%{$fg_bold[yellow]%}$(rbenv version | awk '{print $1}')%{$reset_color%}"
-	else
-	  echo ""
-  fi
-}
-
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo(){
-  if $(which todo.sh &> /dev/null)
-  then
-    num=$(echo $(todo.sh ls +next | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
-    then
-      echo "$todos"
-    else
-      echo ""
-    fi
-  else
-    echo ""
-  fi
-}
-
 directory_name(){
   echo "[%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}]"
 }
 
 export PROMPT=$'$(directory_name)$(git_dirty)› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
-}
 
 precmd() {
   title "zsh" "%m" "%55<...<%~"
-  set_prompt
 }
