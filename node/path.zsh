02b8d189b8af9d7914d815d552b04e5bb962b5c8
Torstein Bjørnstad <tosh@tosh.no>
2013-03-09 15:24:24 +0100

    split out path definitions

---
+++ node/path.zsh
@@ -0,0 +1,4 @@
+npm=/usr/local/share/npm
+if [[ -d $npm ]]
+	then export PATH=$npm/bin:$PATH;
+fi
---
+++ smlnj/path.zsh
@@ -0,0 +1,4 @@
+smlnj=/usr/local/smlnj-110.75
+if [[ -d $smlnj ]]
+	then export PATH=$smlnj/bin:$PATH;
+fi
